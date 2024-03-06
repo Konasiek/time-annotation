a00a4252094b8cbefedc341501b950fb913dafaf
kmerkwa <kmerkwa@software.altkom.pl>
2024-03-06 15:51:40 +0100

    commit ss2

--- src/main/java/org/example/timeannotation/Calculator.java
+++ src/main/java/org/example/timeannotation/Calculator.java
@@ -8,11 +8,5 @@ public class Calculator {
     @MeasureTime
     void calculate() throws InterruptedException {
         Thread.sleep(10000);
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
-        System.out.println("6");
     }
 }
