6a0ce09a764bb8f16f3fdfa44bb2afbc0cf81445
kmerkwa <kmerkwa@software.altkom.pl>
2024-03-06 14:02:23 +0100

    commit 1

---
+++ src/main/java/org/example/timeannotation/TimeAnnotationApplication.java
@@ -0,0 +1,13 @@
+package org.example.timeannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeAnnotationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/timeannotation/TimeAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.timeannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
