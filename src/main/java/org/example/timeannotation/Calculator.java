4add8fa1df84643abd2148c80b129e9a18830225
kmerkwa <kmerkwa@software.altkom.pl>
2024-03-06 15:44:47 +0100

    ss1

--- src/main/java/org/example/timeannotation/Calculator.java
+++ src/main/java/org/example/timeannotation/Calculator.java
@@ -11,5 +11,8 @@ public class Calculator {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
     }
 }
